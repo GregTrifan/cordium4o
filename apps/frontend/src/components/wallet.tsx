b4c1b76d8a7d4be60fa6d76cc8a3bac92a681b9a
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-02-10 23:20:06 +0200

    update wallet look

--- apps/frontend/src/components/wallet.tsx
+++ apps/frontend/src/components/wallet.tsx
@@ -1,9 +1,10 @@
 import { walletPresentAtom, addressPreviewAtom, initiateWalletAtom } from '../store/walletStore';
 import LoginIcon from '@mui/icons-material/Login';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
-import { Button } from '@mui/material';
+import { Button, Stack } from '@mui/material';
 import { useAtomValue, useSetAtom } from 'jotai';
 import React from 'react'
+import { teal } from '@mui/material/colors';
 
 const Wallet = () => {
   const walletPresent = useAtomValue(walletPresentAtom);
@@ -12,9 +13,11 @@ const Wallet = () => {
 
   if (walletPresent)
     return (
-      <Button startIcon={<AccountBalanceWalletIcon />} color="info" variant="contained">
-        {previewAddress}
-      </Button>
+      <Stack direction="row" borderColor={teal[500]} color={teal[500]} border={1} padding="10px" borderRadius="5px" spacing={2} >
+
+        <AccountBalanceWalletIcon />
+        <p>{previewAddress}</p>
+      </Stack >
     )
   return (
     <Button onClick={initiateWallet} startIcon={<LoginIcon />} color="primary" variant="outlined">Connect wallet</Button>
