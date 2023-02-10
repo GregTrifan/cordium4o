c813fdbb5458712042d7400e1fa644433e26fda7
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2023-02-11 01:16:18 +0200

    add select with countries

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -2,6 +2,16 @@ import Navbar from "./components/navbar";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Toaster } from "react-hot-toast";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home";
+import Upload from "./pages/upload";
+import Claim from "./pages/claim";
 
 const darkTheme = createTheme({
   palette: {
@@ -10,11 +20,24 @@ const darkTheme = createTheme({
 });
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<>
+        <Navbar />
+        <Outlet />
+      </>}>
+        <Route path="/" element={<Home />} />
+        <Route path="upload" element={<Upload />} />
+        <Route path="claim/:id" element={<Claim />} />
+      </Route>
+    )
+  );
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Toaster />
       <CssBaseline />
-      <Navbar />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
